5206e242f4c05bfd140ab1ff28c408829f8568e8
Rostyslav Kopotiienko <rost.kop.200.5@gmail.com>
2025-11-20 00:59:36 +0100

    Add model of messages in app messages_app

--- messages_app/models.py
+++ messages_app/models.py
@@ -1,3 +1,21 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="sent_messages"
+    )
+    receiver = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="received_messages"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.content
