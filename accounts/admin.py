ad7ee0129d03b6dd483f2d2a352388b53077f63b
Rostyslav Kopotiienko <rost.kop.200.5@gmail.com>
2025-11-20 01:50:06 +0100

    Add all models of the project to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import (
+    CustomUser,
+    Profile
+)
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "email", "is_staff"]
+    list_filter = ["email", "is_active", "last_name"]
+    search_fields = ["last_name", "email"]
+
+
+@admin.register(Profile)
+class Profile(admin.ModelAdmin):
+    list_display = ["user", "portfolio_website", "avatar"]
+    list_filter = ["portfolio_website"]
+    search_fields = ["user"]
--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import (
+    Project,
+    Application
+)
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["title", "client", "status", "created_at"]
+    list_filter = ["status", "created_at"]
+    search_fields = ["title", "description"]
+
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = ["project", "freelancer", "status", "created_at"]
+    list_filter = ["status", "created_at"]
+    search_fields = ["cover_letter", "status"]
