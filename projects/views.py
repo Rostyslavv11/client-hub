a81b3061689ecc92e710c2e4a7d24b342fc4e74c
Rostyslav Kopotiienko <rost.kop.200.5@gmail.com>
2025-12-21 01:24:06 +0100

    Add detail and list views, use it in templates

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from projects.models import Project, Tags
 
-# Create your views here.
+
+class ProjectsListView(LoginRequiredMixin, ListView):
+    model = Project
+    template_name = "projects/find_work.html"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["tags"] = Tags.objects.all()
+        return context
+
+
+class ProjectDetailView(LoginRequiredMixin, DetailView):
+    model = Project
